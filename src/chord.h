868b8d1fb3b10d58e616a186e96a663e6072a4db
Andrew Carter <andrew.r.carter@gmail.com>
2013-07-31 17:02:56 +0000

    even MORE commenting

--- src/chord.h
+++ src/chord.h
@@ -1,6 +1,8 @@
 #ifndef chord_h
 #define chord_h
 
+// create and push chord display window to stack, using the given
+// string as a lookup for the chords file
 void display_chord(const char *chord);
 
 #endif
